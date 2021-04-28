1ec42d4823fba597922e85d379d92bb22ea2aea5
veero3 <veershah03@gmail.com>
2021-04-28 21:44:38 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports ={
             {name:`**Moderation**`, value:`\`mute, unmute, kick, ban, unban\``},
             {name: `**Role Ulitiies**`, value:`\`role, roleadd, rolerem\``},
             {name: `**Music**`, value:`\`play, stop, skip, queue, leave, pause, resume\``},
-            {name: `**Info**`, value:`userinfo, serverinfo`}
+            {name: `**Info**`, value:`\`userinfo, serverinfo\``}
         )
     //  .setThumbnail(message.guild.icon.link)
     //  .setImage('https://i.imgur.com/wSTFkRM.png')
