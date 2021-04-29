36e2b879f259308167b0fd2dd00863631bd99e08
veero3 <veershah03@gmail.com>
2021-04-29 12:30:31 +0530

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -2,6 +2,7 @@ module.exports={
     name: 'kill',
     aliases: ['die', 'restart'],
     execute(client, message, args, cmd, Discord){
+        message.channel.send('restarting')
         process.exit();
     }
 }
