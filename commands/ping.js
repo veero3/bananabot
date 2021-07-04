32bbd09e28aacefd5fa4b94ffc2777565dd3253e
veero3 <83166010+veero3@users.noreply.github.com>
2021-07-04 23:05:24 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'ping',
     execute(client, message, args, Discord){
-        message.channel.send('pong!');
+        message.channel.send('pinging').then(sent =>{sent.edit(`Pong!!!\n ${sent.createdTimestamp - message.createdTimestamp}ms`)});
+      
+        
 
 
 
