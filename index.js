5d98a7960d03b55e83f319b39849fd3d74432342
veero3 <83166010+veero3@users.noreply.github.com>
2021-07-05 14:32:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,47 +6,16 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" 
 client.commands = new Discord.Collection();
 
 client.events = new Discord.Collection();
+const { DiscordTogether } = require('discord-together');
+
+
+client.discordTogether = new DiscordTogether(client);
 
 
 ['event_handler', 'command_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.login(process.env.token);
+client.login('ODM1NDEyNjkwNDI4MTAwNjQ4.YIPEsg.eDrFKLbLeic7ONk4hecxGWvOIyo');
 
 
-/*const fs = require('fs');
-
-
-
-const commandfiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-
-for(const file of commandfiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-
-client.once('ready', () => {
-    console.log('BOOTED UP');
-});
-
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix)||message.author.bot)return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    }else if(command == 'youtube'){
-        client.commands.get('yt').execute(message, args);
-    }else if(command == 'embed'){
-        client.commands.get('help').execute(message, args, Discord);
-    }else if(command == 'mute'){
-        client.commands.get('mute').execute(message, args);
-    }else if(command == 'unmute'){
-        client.commands.get('unmute').execute(message, args);
-    }
-});*/
