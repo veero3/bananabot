25989b16ef83beccbd4d4eab366a2ef76aad6e08
veero3 <veershah03@gmail.com>
2021-09-19 19:22:37 +0530

    fd

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const profilemodel = require('../../models/profieschema');
 module.exports = async(client, discord, message) =>{
     let profile = new profilemodel({
-        UserID = message.member.id,
+        UserID = user.id,
         ServerID = message.guild.id,
         quantity = 0
         
